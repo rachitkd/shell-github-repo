f604562c90c12e2bf7ead1b9535e8aa37fc29c7b
unknown <rachit.cisco@gmail.com>
2024-08-03 00:13:20 +0530

    Update Frontendsh file

--- expense/Frontend.sh
+++ expense/Frontend.sh
@@ -4,6 +4,8 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
+pwd
+ls -lrth
 cp proxy.conf /etc/nginx/default.d/expense.conf
 systemctl start nginx
 echo "**Frontend Installation is completed**"
